b5a471e32cedf18bda8d8e810360b4771fc71fe9
Jordan Parmentier <joparment@gmail.com>
2025-08-05 19:02:27 +0200

    Ajout deuxième caroussel page gites

--- scripts/gites.js
+++ scripts/gites.js
@@ -1,9 +1,14 @@
 let slideIndex = 1;
 showSlides(slideIndex);
+showSlides2(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
+}
+
+function plusSlides2(n) {
+  showSlides2(slideIndex += n);
 }
 
 // Thumbnail image controls
@@ -11,10 +16,30 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+function currentSlide2(n) {
+  showSlides2(slideIndex = n);
+}
+
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide2");
+  let dots = document.getElementsByClassName("dot2");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {

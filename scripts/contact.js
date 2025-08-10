953a1d186f00eecd2b61ec3a869b7744cf5053d3
Jordan Parmentier <joparment@gmail.com>
2025-08-10 17:04:34 +0200

    background header

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,11 +1,17 @@
-let form = document.querySelector("form")
-form.addEventListener("submit", event => {
+let formResa = document.getElementById("formResa")
+formResa.addEventListener("submit", event => {
     event.preventDefault()
-    gererFormulaire()
+    gererFormulaireResa()
+})
+
+let formRens = document.getElementById("formRens")
+formRens.addEventListener("submit", event => {
+    event.preventDefault()
+    gererFormulaireRens()
 })
 
 
-function gererFormulaire () {
+function gererFormulaireResa () {
     let baliseNom = document.getElementById("nom")
     let nom = baliseNom.value
     let balisePrenom = document.getElementById("prenom")
@@ -20,14 +26,30 @@ function gererFormulaire () {
     let gite = baliseGite.value
     let baliseNbpersonnes = document.getElementById("nbpersonnes")
     let nbpersonnes = baliseNbpersonnes.value
-    let baliseTexte = document.getElementById("texte")
-    let texte = baliseTexte.value
+    let balisePrecisions = document.getElementById("precisions")
+    let precisions = balisePrecisions.value
 
-    envoyerMail (nom, prenom, email, du, au, gite, nbpersonnes, texte)
+    envoyerMailResa (nom, prenom, email, du, au, gite, nbpersonnes, precisions)
 }
 
 
-function envoyerMail (nom, prenom, email, du, au, gite, nbpersonnes, texte) {
+function envoyerMailResa (nom, prenom, email, du, au, gite, nbpersonnes, texte) {
     let mailto = `mailto:vernet.de.danton@gmail.com?subject=Demande de réservation&body=Bonjour, nous souhaiterions réserver le gîte ${gite} du ${du} au ${au} pour ${nbpersonnes} personnes.%0D%0A%0D%0APrécisions : ${texte}%0D%0A%0D%0A${prenom} ${nom}%0D%0A${email}`
+    location.href = mailto
+}
+
+function gererFormulaireRens () {
+    let baliseNom = document.getElementById("nom")
+    let nom = baliseNom.value
+    let balisePrenom = document.getElementById("prenom")
+    let prenom = balisePrenom.value
+    let baliseMessage = document.getElementById("message")
+    let message = baliseMessage.value
+
+    envoyerMailRens (nom, prenom, message)
+}
+
+function envoyerMailRens (nom, prenom, message) {
+    let mailto = `mailto:vernet.de.danton@gmail.com?subject=Demande de réservation&body=${message}%0D%0A%0D%0A${prenom} ${nom}`
     location.href = mailto
 }

080f9e780891e354f4828597e6ef7080a9e1104b
Jordan Parmentier <joparment@gmail.com>
2025-08-17 19:20:17 +0200

    Menu déroulant activités JS =D

--- scripts/activites.js
+++ scripts/activites.js
@@ -1,13 +1,27 @@
-let activitesFamiliales = document.getElementById("activitesFamiliales")
-let activitesFamilialesSection = document.getElementById("activitesFamilialesSection")
 
-activitesFamilialesSection.style.display = "none"
+let activitesList = ["activitesFamiliales", "naturePlanEau", "resto", "produits"]
 
-activitesFamiliales.addEventListener("click", () => {
-    if (activitesFamilialesSection.style.display === "none") {
-        activitesFamilialesSection.style.display = "block"
+
+for (let i = 0; i < activitesList.length; i++) {
+    let activitesSection = document.getElementById(activitesList[i] + "Section")
+    activitesSection.style.display = "none"
+    let activite = document.getElementById(activitesList[i])
+    activite.addEventListener("click", (event) => {
+        console.log(event.target.id)
+        console.log(event.target.id + "Section")
+        let activitesSectionAGerer = document.getElementById(event.target.id + "Section")
+        console.log(activitesSectionAGerer)
+        OuvrirOuFermer(activitesSectionAGerer)
+        })
+    }
+
+
+
+function OuvrirOuFermer (activitesSectionAGerer) {
+    if (activitesSectionAGerer.style.display === "none") {
+        activitesSectionAGerer.style.display = "block"
         }
     else {
-        activitesFamilialesSection.style.display = "none"
+        activitesSectionAGerer.style.display = "none"
     }
-    })
+    }
